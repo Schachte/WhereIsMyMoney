a9565482c6f074ca91d1f9f82eac4d27ef113fd7
schachte <rschacht@asu.edu>
2018-01-08 20:23:56 -0700

    adding final form validation

--- src/Budget/components/Budget.js
+++ src/Budget/components/Budget.js
@@ -9,7 +9,7 @@ const metaProperties = {
 
 const budgetWidgets = [
   'Monthly Budget',
-  'Current Status',
+  'No. Budgets',
   'Amount Left',
   'Avg. Weekly Spent'
 ];
@@ -18,8 +18,12 @@ const BudgetWidgetCard = (props) => {
   return (
     <div className="col-1 col-sm-3 placeholder" style={{float: 'left'}} key={`CARD_${props.i}`}>
       <div className="card card-inverse" style={{backgroundColor: "#333", borderColor: "#333", color: "#FFF", padding: "10px"}}>
-        <div className="card-block">
+        <div className="card-block" style={{align: 'center'}}>
           <h4 className="card-title">{props.title}</h4>
+          <span style={{fontSize: '2em'}}>
+            {props.title == budgetWidgets[0] && <span>${parseFloat(props.state.monthlyBudget).toFixed(2)}</span>}
+            {props.title == budgetWidgets[1] && props.state.numberOfBudgets}
+          </span>
         </div>
       </div>
     </div>
@@ -37,7 +41,7 @@ export default class Budget extends Component {
         <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
           <h1>{metaProperties.pageTitle}</h1>
           {budgetWidgets.map((widget, i) => {
-            return <BudgetWidgetCard title={widget} key={i} />
+            return <BudgetWidgetCard title={widget} key={i} state={this.props} />
           })}
           <div className="card" style={{width: '65%', margin: 'auto', marginTop: '150px'}}>
             <h3 className="card-header">{metaProperties.budgetCardTitle}</h3>
--- src/Budget/containers/BudgetContainer.js
+++ src/Budget/containers/BudgetContainer.js
@@ -8,11 +8,32 @@ import {
   updateBudgetEntry
 } from '..';
 
+/*********************************************************
+Selectors To Pick Apart Redux State At More Granular Level
+*********************************************************/
+export const getBudgetCategories = (state) => state.getIn(['budget', 'budgetCategories']);
+
+export const getNumberOfBudgetCategories = createSelector(
+  getBudgetCategories,
+  (budget) => {
+    return budget.count()
+  }
+)
+
+export const getTotalMonthlyBudget = createSelector(
+   getBudgetCategories,
+   (budget) => budget.toJS().reduce((totalsum, data) => parseInt(data.monthlyCost) + totalsum, 0)
+)
+
+
 const mapStateToProps = state => {
+  console.log(getTotalMonthlyBudget(state))
   return {
     userBudgetItems: state.getIn(['budget', 'budgetCategories']).toJS(),
     budgetFormEditableErrors: state.getIn(['budget', 'budgetFormEditable', 'errors']).toJS()[0],
-    reduxForm: state.getIn(['form']).toJS()
+    reduxForm: state.getIn(['form']).toJS(),
+    numberOfBudgets: getNumberOfBudgetCategories(state),
+    monthlyBudget: getTotalMonthlyBudget(state)
   }
 }
 
