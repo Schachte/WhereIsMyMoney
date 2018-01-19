84de26940531a750d34796df2969731b9fae158d
Ryan Schachte <rschacht@asu.edu>
2018-01-19 13:51:38 -0700

    Unit tests, immutable integration and feature completion for edit budget
    - Successfully sends data from valid form to redux store
    - Tests are passing and running integration test of containers, actions,
    reducers and components
    
    Adding tests and inline form updates
    
    Making immutable feasible and passing automated build
    
    Fixing automated testing to pass redux integration test
    
    Making immutable feasible and passing automated build
    
    Fixing automated testing to pass redux integration test
    
    Adding unit and integration tests for redux in-line form edit
    
    Added working tests for conditional message involving budgets being present or not in rendered table
    
    Writing redux store intgration tests for submitting edit button
    
    Added component tests for editing fields in budget table component
    
    Attempting to update the save/clear options in Immutable
    
    Finished tests for inline-form-edit
    
    Finishing redux integration test for inline form
    
    Merge branch 'master' of https://github.com/Schachte/WhereIsMyMoney into feature/in-form-edit

---
+++ src_client/src/components/budget/BudgetTable.js
@@ -0,0 +1,130 @@
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/no-multi-comp */
+import React, { PropTypes } from 'react';
+import { renderDays } from './AddBudgetForm';
+
+let editedBudget = {
+  budgetCategory: '',
+  budgetCost: '',
+  budgetDate: ''
+};
+
+export const onChangeEdit = (field, data) => {
+  editedBudget[field] = data;
+};
+
+//TODO: REFACTOR THIS DUPLICATED SHIT CODE BELOW
+let getBudgetItems = (budgets, editBudgetFn, editingBudget, clearEditedBudget, updateExistingBudget) => {
+
+  return budgets.map((budget, key) => {
+    if (editingBudget != null &&
+      budget.budgetCategory == editingBudget.budgetCategory) {
+      editedBudget = editingBudget;
+      return (
+        <tr className = "edit-budget-table-row" key={"budget_item_" + key}>
+          <td>
+            <div className="form-group">
+              <input
+                name = "budgetCategoryEdit"
+                className="form-control"
+                placeholder={editedBudget.budgetCategory}
+                onChange={(data) => onChangeEdit("budgetCategory", data.target.value)}
+              />
+            </div>
+          </td>
+          <td>
+            <div className="form-group input-group">
+              <span className="input-group-addon"><i className="fa fa-usd"></i>
+              </span>
+              <input
+                name="budgetCostEdit"
+                className="form-control"
+                type="text"
+                placeholder={editedBudget.budgetCost}
+                onChange={(data) => onChangeEdit("budgetCost", data.target.value)}
+              />
+            </div>
+          </td>
+          <td>
+            <select
+              className="form-control"
+              onChange={(data) => onChangeEdit("budgetDate", data.target.value)}
+            >
+              <option
+                name="budgetDateEdit"
+                defaultValue={editedBudget.budgetDate}
+                >
+                {budget.budgetDate}
+              </option>
+                {renderDays()}
+            </select>
+            </td>
+          <td>
+            <button
+              style={{"marginRight" : "5px"}}
+              name="budget-save-changes"
+              onClick={() => {updateExistingBudget(editedBudget, budget); clearEditedBudget();}}
+              className="btn btn-success">Save Budget
+            </button>
+            <button
+              name="budget-clear-changes"
+              onClick={() => {clearEditedBudget();}}
+              className="btn btn-danger">Cancel Budget
+            </button>
+          </td>
+        </tr>
+      );
+    }
+
+    return (
+      <tr className = "add-budget-table-row" key={"budget_item_" + key}>
+        <td>{budget.budgetCategory}</td>
+        <td>${budget.budgetCost}</td>
+        <td>{budget.budgetDate}</td>
+        <td><button
+          name="budget-edit-btn"
+          onClick={() => {editBudgetFn(budget);}}
+          className="btn btn-primary">Edit Budget</button></td>
+      </tr>
+    );
+  });
+};
+
+const RenderBudgetTable = ({ budgets, editBudget, editingBudget, clearEditedBudget, updateExistingBudget }) => {
+  if (budgets.length == 0) {
+    return (
+      <h2 name="no-budgets-available">
+        You have no budgets saved!<br/> Enter a budget above to view your budget structure
+      </h2>
+    );
+  }
+
+  return (
+    <div className="table-responsive">
+      <table className="table table-hover add-budget-table">
+        <thead>
+          <tr>
+            <th>Budget Name</th>
+            <th>Monthly Cost</th>
+            <th>Due Date</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {getBudgetItems(budgets, editBudget, editingBudget, clearEditedBudget, updateExistingBudget)}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+RenderBudgetTable.propTypes = {
+  budgets : PropTypes.array,
+  editBudget: PropTypes.func,
+  editingBudget: PropTypes.object,
+  clearEditedBudget: PropTypes.func,
+  updateExistingBudget: PropTypes.func
+};
+
+export default RenderBudgetTable;
---
+++ src_client/src/components/budget/tests/BudgetTable.test.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import expect from 'expect';
+import { mount, shallow } from 'enzyme';
+import Budget from '../Budget';
+import AddBudgetForm from '../AddBudgetForm';
+import BudgetTable from '../BudgetTable';
+import Immutable from 'immutable';
+
+import * as actionTypes from '../../../actions/actionTypes';
+import * as budgetActions from '../../../actions/budgetActions';
+import configureStore from '../../../store/configureStore';
+
+/*****************************************
+BUDGET INTEGRATION TEST FOR THE REDUX STORE
+*****************************************/
+let expectedBudget_1 = {
+   budgetCategory: 'TEST_CATEGORY',
+   budgetCost: '5.00',
+   budgetDate: '25'
+ };
+
+let expectedBudget_2 = {
+   budgetCategory: 'TEST_CATEGORY_2',
+   budgetCost: '6.00',
+   budgetDate: '30'
+ };
+
+/*****************************************
+FULL JSDOM COMPONENT RENDER
+*****************************************/
+const setup = (budgets = [], editingBudget = {}) => {
+  let props = {
+   budgets: budgets,
+   editBudget: (budget) => {},
+   editingBudget: editingBudget
+  };
+
+  return shallow(<BudgetTable {...props} />);
+};
+
+/*****************************************
+TESTS
+*****************************************/
+describe("Props render table correctly", () => {
+  let wrapper = setup([expectedBudget_1, expectedBudget_2], [expectedBudget_1]);
+  it("Yields the correct number of rows", () => {
+    let tableElements = wrapper.find('.add-budget-table-row').length;
+    expect(tableElements).toEqual(2);
+  });
+});
+
+describe("Editing different budgets manipulates editable fields", () => {
+  let wrapper = setup([expectedBudget_1, expectedBudget_2], expectedBudget_2);
+
+  it("Ensures that only a single row of items are being edited", () => {
+    expect(wrapper.find('[name="budgetCategoryEdit"]').length).toEqual(1);
+    expect(wrapper.find('[name="budgetCostEdit"]').length).toEqual(1);
+    expect(wrapper.find('[name="budgetDateEdit"]').length).toEqual(1);
+  });
+
+  it("Ensures that the type of editable fields are all correct", () => {
+    expect(wrapper.find('[name="budgetCategoryEdit"]').type()).toBe('input');
+    expect(wrapper.find('[name="budgetCostEdit"]').type()).toBe('input');
+    expect(wrapper.find('[name="budgetDateEdit"]').type()).toBe('option');
+  });
+
+  it("Ensures the edited redux store is the edited row on page", () => {
+    expect(
+      wrapper.find('[name="budgetCategoryEdit"]').find('[placeholder]').props().placeholder
+    ).toEqual(expectedBudget_2.budgetCategory);
+
+    expect(
+      wrapper.find('[name="budgetCostEdit"]').find('[placeholder]').props().placeholder
+    ).toEqual(expectedBudget_2.budgetCost);
+  });
+});
